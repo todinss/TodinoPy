8f8e0a659451e38737a0075d01781d3742325423
todinss <73935659+todinss@users.noreply.github.com>
2021-10-03 13:59:42 +0200

    Add files via upload

---
+++ coniche/retta.py
@@ -0,0 +1,45 @@
+class retta:
+    def __init__(self,a,b,c):
+
+    # un attributo privato si dichiara con il doppio underscore ( __ )    
+        self.__a = a  
+        self.__b = b
+        self.__c = c
+
+    def equazione(self):
+        if(self.__b>0 and self.__c>0):
+            return f'{self.__a}x + {self.__b}y + {self.__c} = 0'
+        elif(self.__b<0 and self.__c>0):
+            return f'{self.__a}x {self.__b}y + {self.__c} = 0'
+        elif(self.__b>0 and self.__c<0):
+            return f'{self.__a}x + {self.__b}y {self.__c} = 0'
+        else:
+            return f'{self.__a}x {self.__b}y {self.__c} = 0'
+
+    def trovaY(self, x):
+        if self.__b!=0:
+            return self.__c/self.__b*-x
+
+    def coppie(self):
+        punti = []
+
+        for i in range(100):
+            punti.append((i,self.trovaY(i)))
+        return punti
+
+
+# inizio del programma chiamante
+
+
+r = retta(-3,-4,-6)
+r2 = retta(3, 4, 6)
+
+print(r.equazione())
+print(r.coppie())
+
+
+print(r2.equazione())
+print(r2.coppie())
+
+# il metodo dir permette di conoscere tutti i metodi applicabili all'oggetto passato come parametro
+print(dir(r))
